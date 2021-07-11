ebd0386ecbd68a22762e3bd87c1f740dd2d70986
Jaiveer Pal <jaiveerpal17@gmail.com>
2021-07-11 17:09:56 +0530

    add dashboard page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 
 function App() {
   return (
     <div className="app-container">
-      <Login />
+      <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+    </Router>
     </div>
   );
 }
---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import "../components/style.css";
+
+function Dashboard() {
+  return (
+    <div className="dashboard-container">
+      Welcome to Dashboard
+    </div>
+  );
+}
+
+export default Dashboard;
